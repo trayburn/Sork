fc9a0f3801c99f7838bce5ebe02e141a9d4af862
Tim Rayburn <tim@timrayburn.net>
2025-01-13 13:59:40 -0600

    Unit testing

---
+++ test/LaughCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Sork.Commands;
+
+namespace Sork.Tests;
+
+[TestClass]
+public sealed class LaughCommandTests
+{
+    [TestMethod]
+    public void Handle_ShouldReturnTrue_WhenInputIsCapitalized()
+    {
+        // Arrange
+        var command = new LaughCommand(new UserInputOutput());
+
+        // Act
+        var result = command.Handles("LOL");
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}
